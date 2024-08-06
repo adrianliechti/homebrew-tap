02431539c9ea7bd16a427fb1cd5c85b2b3b230f7
Adrian Liechti <adrian@monobox.ch>
2024-08-07 00:14:13 +0200

    use metal for mistral.rs

--- mistral.rs.rb
+++ mistral.rs.rb
@@ -1,39 +1,21 @@
 class MistralRs < Formula
-  desc ""
+  desc "Blazingly fast LLM inference"
+
   homepage ""
+  license "MIT"
+
   version "0.2.4"
 
-  on_macos do
-    on_intel do
-      url "https://github.com/EricLBuehler/mistral.rs/releases/download/v0.2.4/mistralrs-server-x86_64-apple-darwin.tar.xz"
-      sha256 "565311cb06cf1cb4c39bd41054006c6ebe15e99e0ea18a1ce5eda70e71365074"
+  url "https://github.com/EricLBuehler/mistral.rs/releases/download/v0.2.4/source.tar.gz"
+  sha256 "f69b6d75ad9d7d54a84a779f8fbcca70a1a61acbc530f3909efbd5111db39e33"
+  
+  depends_on :macos
+  depends_on "rust" => :build
 
-      def install
-        bin.install "mistralrs-server"
-      end
-    end
-
-    on_arm do
-      url "https://github.com/EricLBuehler/mistral.rs/releases/download/v0.2.4/mistralrs-server-aarch64-apple-darwin.tar.xz"
-      sha256 "78bf7c2282901afa73922c8d7070868c3c625c97037f04e391c546739f2b6c36"
-
-      def install
-        bin.install "mistralrs-server"
-      end
-    end
-  end
-
-  on_linux do
-    on_intel do
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/EricLBuehler/mistral.rs/releases/download/v0.2.4/mistralrs-server-x86_64-unknown-linux-gnu.tar.xz"
-        sha256 "fd76e589ed24f94626066da99a848c276d6845bfcd6258a52f25cd5565d534a8"
-
-        def install
-          bin.install "loop"
-        end
-      end
-    end
+  def install
+    ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
+    system "cargo", "build", "--release", "--features", "metal"
+    bin.install "target/release/mistralrs-server"
   end
 
   test do
