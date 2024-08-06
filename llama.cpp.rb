1c3fbe6656d0fe262555b7b05befa7e58454e24c
Adrian Liechti <adrian@monobox.ch>
2024-08-07 00:02:07 +0200

    bump llama

--- llama.cpp.rb
+++ llama.cpp.rb
@@ -4,10 +4,10 @@ class LlamaCpp < Formula
   homepage "https://github.com/ggerganov/llama.cpp"
   license "MIT"
 
-  version "b3490"
+  version "b3534"
 
   url "https://github.com/ggerganov/llama.cpp.git",
-    tag:      "b3490"
+    tag:      "b3534"
 
   depends_on :macos
 
