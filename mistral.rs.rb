d0024bee4f09c5ede96e4036e640ae77e70d5dae
Adrian Liechti <adrian@monobox.ch>
2024-08-16 21:30:42 +0200

    bump mistralrs

--- mistral.rs.rb
+++ mistral.rs.rb
@@ -1,20 +1,20 @@
 class MistralRs < Formula
   desc "Blazingly fast LLM inference"
 
-  homepage ""
+  homepage "https://github.com/EricLBuehler/mistral.rs"
   license "MIT"
 
-  version "0.2.4"
+  version "0.2.5"
 
-  url "https://github.com/EricLBuehler/mistral.rs/releases/download/v0.2.4/source.tar.gz"
-  sha256 "f69b6d75ad9d7d54a84a779f8fbcca70a1a61acbc530f3909efbd5111db39e33"
+  url "https://github.com/EricLBuehler/mistral.rs/releases/download/v0.2.5/source.tar.gz"
+  sha256 "c759348b56fc1954ba3151542b162175e8ba50aa9db3d9c0a6b57f58ebb0e364"
   
   depends_on :macos
   depends_on "rust" => :build
 
   def install
     ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
-    system "cargo", "build", "--release", "--features", "metal"
+    system "cargo", "build", "--release", "--features", "metal", "--package", "mistralrs-server"
     bin.install "target/release/mistralrs-server"
   end
 
