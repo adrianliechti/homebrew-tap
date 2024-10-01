72cc337da54d45dcfb716833788758a6f3b0d753
Adrian Liechti <adrian@monobox.ch>
2024-10-01 17:28:22 +0200

    bump mistral.rs

--- mistral.rs.rb
+++ mistral.rs.rb
@@ -4,10 +4,10 @@ class MistralRs < Formula
   homepage "https://github.com/EricLBuehler/mistral.rs"
   license "MIT"
 
-  version "0.3.0"
+  version "0.3.1"
 
-  url "https://github.com/EricLBuehler/mistral.rs/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "e252fc70c3427ff1cc806d6fc189a5e5d2bffdcc3c2c3a4c207df3107714c5e3"
+  url "https://github.com/EricLBuehler/mistral.rs/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "61af829fd474e666fac50a80d08df02f0bec9eca4cc992902f2df62932fe6716"
   
   depends_on :macos
   depends_on "rust" => :build
