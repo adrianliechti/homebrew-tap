d3da85d1ef0bf857727c0cd31de33a07a085a9e3
Adrian Liechti <adrian@monobox.ch>
2024-11-09 19:00:25 +0100

    bump llama.cpp

--- llama.cpp.rb
+++ llama.cpp.rb
@@ -4,15 +4,15 @@ class LlamaCpp < Formula
   homepage "https://github.com/ggerganov/llama.cpp"
   license "MIT"
 
-  version "b4041"
+  version "b4061"
 
-  url "https://github.com/ggerganov/llama.cpp.git", tag: "b4041"
+  url "https://github.com/ggerganov/llama.cpp.git", tag: "b4061"
 
   depends_on :macos
 
   def install
-    system "make", "LLAMA_CURL=1", "LLAMA_METAL=1", "LLAMA_METAL_EMBED_LIBRARY=1", "llama-cli"
-    system "make", "LLAMA_CURL=1", "LLAMA_METAL=1", "LLAMA_METAL_EMBED_LIBRARY=1", "llama-server"
+    system "make", "LLAMA_CURL=1", "GGML_METAL=1", "GGML_METAL_EMBED_LIBRARY=1", "llama-cli"
+    system "make", "LLAMA_CURL=1", "GGML_METAL=1", "GGML_METAL_EMBED_LIBRARY=1", "llama-server"
     bin.install "llama-cli"
     bin.install "llama-server"
   end
