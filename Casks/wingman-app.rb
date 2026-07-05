f83ed1508af2ffba6d2e9a7410e29920e320c6c7
Adrian Liechti <adrian@localhost>
2026-07-06 01:01:34 +0200

    wingman-app 0.10.0

--- Casks/wingman-app.rb
+++ Casks/wingman-app.rb
@@ -1,6 +1,6 @@
 cask "wingman-app" do
-  version "0.9.9"
-  sha256 "9f47bdb4028538592cdc4433bce43af227c2d0010dea635e227fcffde120aacd"
+  version "0.10.0"
+  sha256 "6fd3cb0d170006807d0cfa1b6ed7fc2449f5a43f45665d71c14b83bb30cd0911"
 
   url "https://github.com/adrianliechti/wingman-agent/releases/download/v#{version}/wingman-app_#{version}_macOS_arm64.zip"
   name "Wingman Agent"
